cdb89933b1d0668364523c9ee7472c6f89a29161
vasanthdachepalli <vasanthdachepalli31@gmail.com>
2023-11-09 11:22:13 +0530

    monthly records added but not integrated still,database work done,server not done

--- controllers/deleter.js
+++ controllers/deleter.js
@@ -1,7 +1,17 @@
 const transtion_daily = require('../models/transctiondaily');
+const catogerywisedaily =require('../models/catoregywisedaily');
 const date = require('../api/dategenerater');
+const dat1 = new Date();
 module.exports = function(req,res,next){
 transtion_daily.deleteMany({date:{$ne : date()}})
+.then(()=>{
+    console.log('deleted successfully');
+})
+.catch((err)=>{
+    console.log(err);
+})
+
+catogerywisedaily.deleteMany({monthnumber:{$ne : dat1.getMonth}})
 .then(()=>{
     console.log('deleted successfully');
 })
